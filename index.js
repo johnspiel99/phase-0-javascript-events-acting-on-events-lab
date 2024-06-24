f75ab46722d244e42421555212b425657bd83d68
Johnspiel <njuejohnspiel@gmail.com>
2024-06-24 07:49:18 +0300

    New

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+
+// DODGER to the left
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// DODGER to the right
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) { 
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+module.exports = {
+    moveDodgerLeft,
+    moveDodgerRight
+};
